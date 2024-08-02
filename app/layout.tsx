37b52a3f7898e7d7d3973acb06d820196e297809
Damni Saini <damnisaini04@gmail.com>
2024-08-02 11:51:50 +0530

    add changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
+           
           },
           variables: {
             colorText: "#fff",
